d316186975eb612af158cfccdfcfb8d948e47ecb
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-09-18 17:57:51 -0400

    Update front page of blog site

--- portfolio-blog/src/app/page.tsx
+++ portfolio-blog/src/app/page.tsx
@@ -6,10 +6,6 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Welcome to my blog m%#&!f^cker
-          </li>
-          <li className="tracking-[-.01em]">Saving my changes soon b!@#$</li>
-          <li className="mb-2 tracking-[-.01em]">
             <Link href="/blog"> blog</Link>
           </li>
         </ol>
